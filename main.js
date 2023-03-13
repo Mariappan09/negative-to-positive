9a952b11f19286f4df518ebcea9b869c975f6f98
Your Name <youamariappan2001@example.com>
2023-03-13 21:31:05 +0530

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+var a=[2,3,4,-2,-7,-8,-3];
+ var b=[];
+ for(var i=0;i<a.length;i++)
+ {
+	 if(a[i]<0)
+	 {
+		 b.push(a[i]*-1)
+	}
+ }
+ console.log(b);
+var a=[4,-3,5,-7,9,-8];
+var b=[];
+for(var i=0	;i<a.length;i++)
+{
+	if(a[i]>0)
+	{
+		b.push(a[i]*-1)
+	}
+}
+console.log(b);
+
+
+
